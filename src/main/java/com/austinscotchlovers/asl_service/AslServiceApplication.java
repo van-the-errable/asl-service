b7e61961f667411fbe6c3d009948762e78edf980
Stevan Betcher <smbetcher@gmail.com>
2025-08-27 00:40:02 -0500

    Initial commit

---
+++ src/main/java/com/austinscotchlovers/asl_service/AslServiceApplication.java
@@ -0,0 +1,13 @@
+package com.austinscotchlovers.asl_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AslServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AslServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/austinscotchlovers/asl_service/AslServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.austinscotchlovers.asl_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AslServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
